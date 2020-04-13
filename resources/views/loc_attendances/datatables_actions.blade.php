0c34cebed87cb6229f7dc7caa15aa13876c43fb6
Dwi Wijonarko <d.wijonarko@gmail.com>
2020-04-13 18:27:35 +0700

    remove edit and delete button if not admin

--- resources/views/loc_attendances/datatables_actions.blade.php
+++ resources/views/loc_attendances/datatables_actions.blade.php
@@ -3,13 +3,16 @@
     <a href="{{ route('locAttendances.show', $id) }}" class='btn btn-ghost-success'>
        <i class="fa fa-eye"></i>
     </a>
+    @can('isAdmin')
     <a href="{{ route('locAttendances.edit', $id) }}" class='btn btn-ghost-info'>
-       <i class="fa fa-edit"></i>
-    </a>
-    {!! Form::button('<i class="fa fa-trash"></i>', [
-        'type' => 'submit',
-        'class' => 'btn btn-ghost-danger',
-        'onclick' => "return confirm('Are you sure?')"
-    ]) !!}
+        <i class="fa fa-edit"></i>
+     </a>
+     {!! Form::button('<i class="fa fa-trash"></i>', [
+         'type' => 'submit',
+         'class' => 'btn btn-ghost-danger',
+         'onclick' => "return confirm('Are you sure?')"
+     ]) !!}        
+    @endcan
+
 </div>
 {!! Form::close() !!}
